689b04537b192849658ffe001dc93b4073661767
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-06-26 00:22:04 +0200

    Commence actual source file

---
+++ wecon.js
@@ -0,0 +1,29 @@
+/* wecon -- Web Console emulator
+ * https://github.com/CylonicRaider/wecon */
+
+/* Hide implementation details in namespace */
+this.Terminal = function() {
+  /* Actual terminal emulator. options specifies parameters of the terminal:
+   * width     : The terminal should have the given (fixed) width; if not set,
+   *             it will adapt to the container.
+   * height    : Fixed height.
+   * bell      : An <audio> element (or anything having a play() method) that
+   *             should be invoked for a BEL character.
+   * visualBell: If true, bells will be indicated by shortly flashing the
+   *             terminal output, independently from bell.
+   * scrollback: Maximum length of the scrollback buffer. When not set, all
+   *             lines "above" the display area are immediately discarded;
+   *             when set to positive infinity, arbitrarily many lines are
+   *             stored.
+   */
+  function Terminal(options) {
+    this.width = options.width;
+    this.height = options.height;
+    this.bell = options.bell;
+    this.visualBell = options.visualBell;
+    this.scrollback = options.scrollback;
+  }
+
+  /* Return export */
+  return Terminal;
+}();
