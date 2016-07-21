CylonicRaider/wecon

commit 34c05cbb3df300f5d72ba82c331bd6ed3009b8ee

Finish display attributes

wecon.js
@@ -248,6 +248,7 @@ this.Terminal = function() {
         this.unmount();
       } else {
         node.innerHTML = "<pre></pre>";
+        this._prepareAttrs()(node.firstElementChild);
       }
       this.node = node;
       node.classList.add("wecon");
@@ -435,10 +436,14 @@ this.Terminal = function() {
       /* Obtain contents and lines */
       var content = this.node.querySelector("pre");
       var lines = content.children;
+      var attrs = this._prepareAttrs(content);
       /* Amend lines */
       var fullLength = this._offscreenLines + y + 1;
-      while (lines.length < fullLength)
-        content.appendChild(makeNode("div"));
+      while (lines.length < fullLength) {
+        var node = makeNode("div");
+        attrs(node);
+        content.appendChild(node);
+      }
       /* Update _offscreenLines */
       if (y >= this.size[1]) {
         this._offscreenLines += y - this.size[1] + 1;
@@ -486,12 +491,14 @@ this.Terminal = function() {
      */
     growCells: function(line, x, pad) {
       this.checkMounted();
+      var attrs = this._prepareAttrs(line);
       var children = line.children, cell;
       for (var i = 0; i <= x; i++) {
         cell = children[i];
         if (! cell) {
           cell = this._cells.get();
           line.appendChild(cell);
+          attrs(cell);
         }
         if (pad && ! cell.textContent) cell.textContent = " ";
       }
@@ -542,7 +549,7 @@ this.Terminal = function() {
      * If pos or any part of it is missing, the cursor position (or the
      * corresponding part of it) is reported.
      * The position is ensured to be within the terminal's bounds.
-     * The .bg, .fg, and .attrs are replaced by their counterparts
+     * The .fg, .bg, and .attrs are replaced by their counterparts
      * from this if not present (=== undefined).
      */
     _resolvePosition: function(pos) {
@@ -559,12 +566,36 @@ this.Terminal = function() {
       return pos;
     },
 
+    /* Resolve the given Y coordinate WRT the current cursor position
+     * The .fg, .bg, .attrs attributes are filled in if necessary.
+     */
+    _resolveY: function(y) {
+      /* Turn into an object */
+      if (Array.isArray(y)) y = y[1];
+      if (y == null) {
+        y = new Number(this.curPos[1]);
+      } else if (typeof y == "number") {
+        y = new Number(y);
+      } else if (Array.isArray(y)) {
+        y = new Number(y[1]);
+      }
+      /* Insert attributes */
+      if (y.fg === undefined) y.fg = this.curFg;
+      if (y.bg === undefined) y.bg = this.curBg;
+      if (y.attrs === undefined) y.attrs = this.curAttrs;
+      /* Done */
+      return y;
+    },
+
     /* Return a closure which assigns the attributes as configured by base
      * to any cell it's called on
+     * If base is null, the terminal's current attributes are used.
      * If base is a DOM node, its data-attrs attribute is applied.
      */
     _prepareAttrs: function(base) {
       var attrs = "";
+      if (base == null)
+        base = {attrs: this.curAttrs, fg: this.curFg, bg: this.curBg};
       if (typeof base == "object" && base.nodeType !== undefined) {
         attrs = base.getAttribute("data-attrs");
       } else {
@@ -573,8 +604,16 @@ this.Terminal = function() {
           if (base.attrs & i) attrs += " " + Terminal.ATTRNAME[i];
         }
         /* Foreground and background */
-        if (base.fg != null) attrs += " fg-" + base.fg;
-        if (base.bg != null) attrs += " bg-" + base.bg;
+        if (base.fg != null) {
+          attrs += " fg-" + base.fg;
+        } else {
+          attrs += " fg-default";
+        }
+        if (base.bg != null) {
+          attrs += " bg-" + base.bg;
+        } else {
+          attrs += " bg-default";
+        }
         /* Strip leading space */
         attrs = attrs.replace(/^ /, "");
       }
@@ -746,6 +785,7 @@ this.Terminal = function() {
     eraseLine: function(before, after, pos) {
       /* Resolve position */
       pos = this._resolvePosition(pos);
+      var attrs = this._prepareAttrs(pos);
       /* Determine line and bounds */
       var line = this.getLine(pos[1]);
       if (! line) return;
@@ -759,9 +799,12 @@ this.Terminal = function() {
           this._cells.add(el);
           line.removeChild(el);
         }.bind(this));
+        /* Assign attributes */
+        attrs(line);
       } else {
         range.forEach(function(el) {
           el.textContent = " ";
+          attrs(el);
         });
       }
       /* Ensure we did not remove the cursor */
@@ -774,6 +817,7 @@ this.Terminal = function() {
       this.checkMounted();
       /* Resolve position */
       var pos = this._resolvePosition(pos);
+      var attrs = this._prepareAttrs(pos);
       /* Obtain reference to line array */
       var content = this.node.querySelector("pre");
       var lines = content.children;
@@ -782,15 +826,19 @@ this.Terminal = function() {
       /* Clear lines above or below */
       if (before) {
         for (var y = 0; y < pos[1]; y++) {
-          this._clearLine(lines[this._offscreenLines + y], true);
+          var ln = lines[this._offscreenLines + y];
+          this._clearLine(ln, true);
+          attrs(ln);
         }
       }
       if (after) {
         var fl = this._offscreenLines + pos[1] + 1;
         while (lines.length > fl) {
-          this._clearLine(content.lastElementChild, false);
-          content.removeChild(content.lastElementChild);
+          var ln = content.lastElementChild;
+          this._clearLine(ln, false);
+          content.removeChild(ln);
         }
+        attrs(content);
       }
       /* Clear scrollback */
       if (scrollback) {
@@ -832,6 +880,19 @@ this.Terminal = function() {
       }
       /* Make book-keeping DOM changes */
       this._updatePadding();
+    },
+
+    /* Set the current display attributes to the given values
+     * fg and bg are color indices or null, attrs is a bitmask of the
+     * Terminal.ATTR.* constants.
+     * If fg or bg are truly (===) undefined, the corresponding values
+     * are unchanged, attrs can be weakly equal to undefined (i.e. null)
+     * not to affect the current value.
+     */
+    setAttributes: function(fg, bg, attrs) {
+      if (fg !== undefined) this.curFg = fg;
+      if (bg !== undefined) this.curBg = bg;
+      if (attrs != undefined) this.curAttrs = attrs;
     }
   };
 

wecon-test.html
@@ -27,18 +27,12 @@ h1 {
 window.addEventListener('load', function() {
   window.term = new Terminal({width: 80, height: 25, scrollback: 1000});
   term.mount(document.getElementById('terminal'));
-  var s = "";
-  for (var i = 0; i < 200; i++) {
-    if (i) s += " ";
-    s += "Hello World!";
-  }
-  term.writeTextRaw(s);
-  term.placeCursor(40, 0);
+  term.setAttributes(15, 4);
+  term.placeCursor(40, 10);
+  term.eraseDisplay(true, false);
+  term.setAttributes(15, 1, Terminal.ATTR.ITALIC);
   term.eraseDisplay(false, true);
-  term.newLine(true, true, true);
-  var pos = [null, null];
-  pos.attrs = Terminal.ATTR.BOLD;
-  term.writeTextRaw("I am bold!", pos);
+  term.writeTextRaw("Hello World!");
 });
     </script>
   </head>

wecon.css
@@ -110,3 +110,38 @@
 .wecon [data-attrs~=dblunderline] {
   text-decoration: double underline;
 }
+
+.wecon [data-attrs~=fg-default] { color:      #AAAAAA; }
+.wecon [data-attrs~=bg-default] { background: #000000; }
+.wecon [data-attrs~=fg-0]  { color:      #000000; }
+.wecon [data-attrs~=bg-0]  { background: #000000; }
+.wecon [data-attrs~=fg-1]  { color:      #AA0000; }
+.wecon [data-attrs~=bg-1]  { background: #AA0000; }
+.wecon [data-attrs~=fg-2]  { color:      #00AA00; }
+.wecon [data-attrs~=bg-2]  { background: #00AA00; }
+.wecon [data-attrs~=fg-3]  { color:      #AA5500; }
+.wecon [data-attrs~=bg-3]  { background: #AAAA00; }
+.wecon [data-attrs~=fg-4]  { color:      #0000AA; }
+.wecon [data-attrs~=bg-4]  { background: #0000AA; }
+.wecon [data-attrs~=fg-5]  { color:      #AA00AA; }
+.wecon [data-attrs~=bg-5]  { background: #AA00AA; }
+.wecon [data-attrs~=fg-6]  { color:      #00AAAA; }
+.wecon [data-attrs~=bg-6]  { background: #00AAAA; }
+.wecon [data-attrs~=fg-7]  { color:      #AAAAAA; }
+.wecon [data-attrs~=bg-7]  { background: #AAAAAA; }
+.wecon [data-attrs~=fg-8]  { color:      #555555; }
+.wecon [data-attrs~=bg-8]  { background: #555555; }
+.wecon [data-attrs~=fg-9]  { color:      #FF5555; }
+.wecon [data-attrs~=bg-9]  { background: #FF5555; }
+.wecon [data-attrs~=fg-10] { color:      #55FF55; }
+.wecon [data-attrs~=bg-10] { background: #55FF55; }
+.wecon [data-attrs~=fg-11] { color:      #FFFF55; }
+.wecon [data-attrs~=bg-11] { background: #FFFF55; }
+.wecon [data-attrs~=fg-12] { color:      #5555FF; }
+.wecon [data-attrs~=bg-12] { background: #5555FF; }
+.wecon [data-attrs~=fg-13] { color:      #FF55FF; }
+.wecon [data-attrs~=bg-13] { background: #FF55FF; }
+.wecon [data-attrs~=fg-14] { color:      #55FFFF; }
+.wecon [data-attrs~=bg-14] { background: #55FFFF; }
+.wecon [data-attrs~=fg-15] { color:      #FFFFFF; }
+.wecon [data-attrs~=bg-15] { background: #FFFFFF; }

commit 2b587a79ff531d32cfc63395aa229d2a781b6ae2

Fix issue
Actually use _resolveY as introduced in 34c05cb.

wecon.js
@@ -837,7 +837,7 @@ this.Terminal = function() {
     spliceLines: function(y, remove, insert) {
       this.checkMounted();
       /* Resolve coordinate */
-      if (y == null) y = this.curPos[1];
+      y = this._resolveY(y);
       var content = this._contentNode();
       var lines = content.children;
       /* Remove lines */